5fe73a0c2f966ad4c6c5196ad97f2041ff836b97
laura.ashileyqueiroz@gmail.com <miguel.ditanno.vigano.1.3@gmail.com>
2026-03-17 20:45:58 -0300

    'funcionando'

--- app_tarefa/urls.py
+++ app_tarefa/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app_tarefa.views import index
+from app_tarefa.views import index, tarefas
 
 urlpatterns = [
-   path('',index, name='index')
+   path('',index, name='index'),
+   path('tarefas', tarefas, name='tarefas'),
+
 ]
--- app_tarefa/views.py
+++ app_tarefa/views.py
@@ -5,19 +5,6 @@ from django.contrib.auth import authenticate, login
 def index(request):
     return render(request, 'app_tarefa/index.html')
 
-def login_view(request):
-    if request.method == 'POST':
-        # Aqui o Django pega os dados do formulário
-        user_nome = request.POST.get('username')
-        user_pass = request.POST.get('password')
-        
-        # Simulação de autenticação (ou use o sistema do Django)
-        user = authenticate(request, username=user_nome, password=user_pass)
-        
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard') # Redireciona para a URL chamada 'dashboard'
-        else:
-            return render(request, 'index.html', {'erro': 'Usuário inválido'})
+def tarefas(request):  
+    return render(request, 'app_tarefa/dashboard.html')
 
-    return render(request, 'index.html')
