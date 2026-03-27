8f58f6beb5035d435b25df67cdf0a36141f58ec8
laura.ashileyqueiroz@gmail.com <miguel.ditanno.vigano.1.3@gmail.com>
2026-03-27 20:47:41 -0300

    continuação

--- app_tarefa/urls.py
+++ app_tarefa/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from app_tarefa.views import index, tarefas
+from app_tarefa.views import index, tarefas, create_novo
 
 urlpatterns = [
    path('',index, name='index'),
    path('tarefas', tarefas, name='tarefas'),
+   path('create', create_novo, name='create_novo'),
+   path('editar/<int:id>', create_novo, name='editar_tarefa'),
+   path('excluir/<int:id>', create_novo, name='excluir_tarefa'),
 
 ]
--- app_tarefa/views.py
+++ app_tarefa/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from app_tarefa.models import Tarefa
 
 def index(request):
     return render(request, 'app_tarefa/index.html')
 
 def tarefas(request):  
-    return render(request, 'app_tarefa/dashboard.html')
+    tarefas = Tarefa.objects.all()
+    return render(request, 'app_tarefa/dashboard.html', {'tarefas': tarefas})
 
+def create_novo(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        status = False
+
+        Tarefa.objects.create(titulo=titulo, descricao=descricao, data=data, status=status)
+        return redirect('tarefas')
+    return render(request, 'app_tarefa/create_novo.html')
